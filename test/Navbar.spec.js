33bcec912542215bee0b049af0158df6956137da
Ben Crane <bencrane5.12@gmail.com>
2019-11-05 09:28:07 -0500

    Update test

--- test/Navbar.spec.js
+++ test/Navbar.spec.js
@@ -1,9 +1,13 @@
-import { mount } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import store from '@/store/index';
 import Navbar from '@/components/Navbar.vue';
 
 describe('Navbar', () => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
     test('is a Vue instance', () => {
-        const wrapper = mount(Navbar);
+        const wrapper = mount(Navbar, { store, localVue });
         expect(wrapper.isVueInstance()).toBeTruthy();
     });
 });
