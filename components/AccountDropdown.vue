de54d91072d67a09de9ef13de43260de93dce544
Ben Crane <bencrane5.12@gmail.com>
2019-11-04 22:58:45 -0500

    Navbar theming

--- components/AccountDropdown.vue
+++ components/AccountDropdown.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="relative">
         <button
-            class="relative z-10 block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white"
+            class="text-white relative z-10 block h-8 w-8 rounded-full overflow-hidden focus:outline-none"
             @click="isOpen = !isOpen"
         >
-            <img
-                class="h-full w-full object-cover"
-                src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
-                alt="Your avatar"
-            />
+            <svg class="fill-current h-full w-full object-cover">
+                <path
+                    d="M25,13c0-6.6166992-5.3828125-12-12-12S1,6.3833008,1,13c0,3.383606,1.413208,6.4386597,3.673645,8.6222534  c0.0529175,0.0689087,0.1156006,0.1247559,0.1889648,0.171814C7.0038452,23.7769165,9.8582764,25,13,25  s5.9961548-1.2230835,8.1373901-3.2059326c0.0733643-0.0470581,0.1360474-0.1029053,0.1889648-0.171814  C23.586792,19.4386597,25,16.383606,25,13z M13,2.5c5.7900391,0,10.5,4.7104492,10.5,10.5  c0,2.4549561-0.8532715,4.7108154-2.2702637,6.5008545c-0.6505127-2.0978394-2.5076294-3.7401123-5.0281372-4.4957886  c1.3735962-0.9940796,2.2720337-2.6046143,2.2720337-4.4244995c0-3.0141602-2.4550781-5.4663086-5.4736328-5.4663086  s-5.4736328,2.4521484-5.4736328,5.4663086c0,1.8198853,0.8984375,3.4304199,2.2720337,4.4244995  c-2.5205078,0.7556763-4.3776245,2.3979492-5.0281372,4.4957886C3.3532715,17.7108154,2.5,15.4549561,2.5,13  C2.5,7.2104492,7.2099609,2.5,13,2.5z M9.0263672,10.5805664c0-2.1870117,1.7822266-3.9663086,3.9736328-3.9663086  s3.9736328,1.7792969,3.9736328,3.9663086S15.1914063,14.546875,13,14.546875S9.0263672,12.7675781,9.0263672,10.5805664z   M6.0307617,20.8319702C6.2562256,18.0820313,9.1723633,16.046875,13,16.046875s6.7437744,2.0351563,6.9692383,4.7850952  C18.1130981,22.4855347,15.6757202,23.5,13,23.5S7.8869019,22.4855347,6.0307617,20.8319702z"
+                />
+            </svg>
         </button>
         <button
             v-if="isOpen"
@@ -22,21 +22,21 @@
         >
             <nuxt-link
                 to="#"
-                class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
+                class="block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white"
             >
                 Account settings
             </nuxt-link>
             <nuxt-link
                 to="#"
-                class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
+                class="block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white"
             >
                 Support
             </nuxt-link>
             <nuxt-link
-                to="#"
-                class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white"
+                to="/auth/logout"
+                class="block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white"
             >
-                Sign out
+                Log out
             </nuxt-link>
         </div>
     </div>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,7 +3,7 @@
         class="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3"
     >
         <div class="flex items-center justify-between px-4 py-3 sm:p-0">
-            <div class="text-white">
+            <div class="text-white font-semibold text-xl">
                 <nuxt-link to="/">Finances</nuxt-link>
             </div>
             <div class="sm:hidden">
@@ -30,33 +30,34 @@
         <nav :class="isOpen ? 'block' : 'hidden'" class="sm:block">
             <div class="px-2 pt-2 pb-4 sm:flex sm:p-0">
                 <nuxt-link
-                    to="#"
+                    v-if="isAuthenticated"
+                    to="/dashboard"
                     class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
                 >
-                    List your property
+                    Dashboard
                 </nuxt-link>
                 <nuxt-link
-                    to="#"
-                    class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+                    v-if="!isAuthenticated"
+                    to="/auth/login"
+                    class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
                 >
-                    Trips
+                    Log In
                 </nuxt-link>
-                <nuxt-link
-                    to="#"
-                    class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-                >
-                    Messages
-                </nuxt-link>
-                <AccountDropdown class="hidden sm:block sm:ml-6" />
+                <AccountDropdown
+                    v-if="isAuthenticated"
+                    class="hidden sm:block sm:ml-6"
+                />
             </div>
-            <div class="px-4 py-5 border-t border-gray-800 sm:hidden">
+            <div
+                v-if="isAuthenticated"
+                class="px-4 py-5 border-t border-gray-800 sm:hidden"
+            >
                 <div class="flex items-center">
-                    <img
-                        class="h-8 w-8 border-2 border-gray-600 rounded-full object-cover"
-                        src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
-                        alt="Your avatar"
-                    />
-                    <span class="ml-3 font-semibold text-white">Jane Doe</span>
+                    <svg class="fill-current text-white h-8 w-8 object-cover">
+                        <path
+                            d="M25,13c0-6.6166992-5.3828125-12-12-12S1,6.3833008,1,13c0,3.383606,1.413208,6.4386597,3.673645,8.6222534  c0.0529175,0.0689087,0.1156006,0.1247559,0.1889648,0.171814C7.0038452,23.7769165,9.8582764,25,13,25  s5.9961548-1.2230835,8.1373901-3.2059326c0.0733643-0.0470581,0.1360474-0.1029053,0.1889648-0.171814  C23.586792,19.4386597,25,16.383606,25,13z M13,2.5c5.7900391,0,10.5,4.7104492,10.5,10.5  c0,2.4549561-0.8532715,4.7108154-2.2702637,6.5008545c-0.6505127-2.0978394-2.5076294-3.7401123-5.0281372-4.4957886  c1.3735962-0.9940796,2.2720337-2.6046143,2.2720337-4.4244995c0-3.0141602-2.4550781-5.4663086-5.4736328-5.4663086  s-5.4736328,2.4521484-5.4736328,5.4663086c0,1.8198853,0.8984375,3.4304199,2.2720337,4.4244995  c-2.5205078,0.7556763-4.3776245,2.3979492-5.0281372,4.4957886C3.3532715,17.7108154,2.5,15.4549561,2.5,13  C2.5,7.2104492,7.2099609,2.5,13,2.5z M9.0263672,10.5805664c0-2.1870117,1.7822266-3.9663086,3.9736328-3.9663086  s3.9736328,1.7792969,3.9736328,3.9663086S15.1914063,14.546875,13,14.546875S9.0263672,12.7675781,9.0263672,10.5805664z   M6.0307617,20.8319702C6.2562256,18.0820313,9.1723633,16.046875,13,16.046875s6.7437744,2.0351563,6.9692383,4.7850952  C18.1130981,22.4855347,15.6757202,23.5,13,23.5S7.8869019,22.4855347,6.0307617,20.8319702z"
+                        />
+                    </svg>
                 </div>
                 <div class="mt-4">
                     <nuxt-link
@@ -72,10 +73,10 @@
                         Support
                     </nuxt-link>
                     <nuxt-link
-                        to="#"
+                        to="/auth/logout"
                         class="mt-2 block text-gray-400 hover:text-white"
                     >
-                        Sign out
+                        Log out
                     </nuxt-link>
                 </div>
             </div>
@@ -101,9 +102,9 @@ export default {
             return this.$store.getters.isAuthenticated;
         }
     },
-    methods: {
-        menuClick() {
-            this.showMenu = !this.showMenu;
+    watch: {
+        $route(to, from) {
+            this.isOpen = false;
         }
     }
 };
