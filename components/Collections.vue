407cfc9121b7295dec68f28379a69f3c2d8b47e2
Ben Crane <bencrane5.12@gmail.com>
2019-11-27 18:37:10 -0500

    Update component

--- components/Collections.vue
+++ components/Collections.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
         Collections
+        {{ collections }}
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Collections'
+    name: 'Collections',
+    data: () => {
+        return {
+            collections: []
+        };
+    },
+    async created() {
+        const { data } = await this.$axios.get(
+            'https://baintanqvd.execute-api.us-east-1.amazonaws.com/dev/hello'
+        );
+        this.collections = data;
+    }
 };
 </script>
